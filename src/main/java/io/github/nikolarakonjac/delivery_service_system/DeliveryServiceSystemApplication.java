940bfbac39874d97d4536e2c2066ebab4c040a3d
nikola-rakonjac <rakonjacnikola8@gmail.com>
2026-06-22 15:37:10 +0200

    project base

---
+++ src/main/java/io/github/nikolarakonjac/delivery_service_system/DeliveryServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.nikolarakonjac.delivery_service_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nikolarakonjac/delivery_service_system/DeliveryServiceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nikolarakonjac.delivery_service_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
